c2836278476d9793bbc7bab3d2aeef45731a257d
manaki <32151765+manak1@users.noreply.github.com>
2019-08-02 00:49:13 +0900

    Add files via upload

---
+++ assets/js/common.js
@@ -0,0 +1,16 @@
+$(function () {
+
+
+    $(window).load(function () {
+
+        $('#loader').slideUp();
+
+        let scene = document.getElementById('demo');
+        let parallaxINstance = new Parallax(scene);
+
+
+
+    })
+
+
+})
