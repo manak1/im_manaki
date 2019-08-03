1dc12bd63caca2c1788c1863350ae92f264be11d
manaki <32151765+manak1@users.noreply.github.com>
2019-08-03 14:38:30 +0900

    Add files via upload

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,7 +3,15 @@ $(function () {
 
     $(window).load(function () {
 
-        $('#loader').slideUp();
+        new fullpage('#fullPage', {
+            autoScrolling: true,
+            navigation: true,
+            navigationPosition: 'left',
+        })
+
+        $('#loader').slideUp(700, 'easeInQuint');
+
+
 
         let scene = document.getElementById('demo');
         let parallaxINstance = new Parallax(scene);
