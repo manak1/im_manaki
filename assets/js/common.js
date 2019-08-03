abc79bfa411241ec969dae79e6c66ef386cbad54
manaki <32151765+manak1@users.noreply.github.com>
2019-08-03 22:12:46 +0900

    Add files via upload

--- assets/js/common.js
+++ assets/js/common.js
@@ -10,11 +10,26 @@ $(function () {
         })
 
         $('#loader').slideUp(700, 'easeInQuint');
-
-
-
         let scene = document.getElementById('demo');
         let parallaxINstance = new Parallax(scene);
+
+
+
+
+
+
+        var test1 = anime({
+            targets: '.main__sun-img',
+            loop: false,
+            easing: 'easeOutCubic',
+            opacity: [0, 1],
+            translateX: ['100%', 0],
+            translateZ: 0,
+            duration: 850,
+            delay: function (el, i) {
+                return 500 + 50 * i
+            }
+        });
 
 
 
